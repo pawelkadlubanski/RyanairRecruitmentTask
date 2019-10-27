493c616d87297cc52988b9ecf30f25007268195b
Pawel Kadlubanski <pawel.kadlubanski@gmail.com>
2019-10-27 22:36:34 +0100

    Add controller.

--- src/main/java/com/ryanair/flight/controller/FlightsServiceController.java
+++ src/main/java/com/ryanair/flight/controller/FlightsServiceController.java
@@ -1,12 +1,17 @@
 package com.ryanair.flight.controller;
 
+import com.ryanair.flight.domain.Flight;
+import com.ryanair.flight.service.FlightService;
 import lombok.RequiredArgsConstructor;
 import lombok.extern.slf4j.Slf4j;
 import org.springframework.http.MediaType;
 import org.springframework.http.ResponseEntity;
-import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
 import org.springframework.web.bind.annotation.RestController;
+
+import java.util.List;
 
 @RestController
 @RequestMapping(
@@ -16,11 +21,15 @@ import org.springframework.web.bind.annotation.RestController;
 @Slf4j
 @RequiredArgsConstructor
 public class FlightsServiceController {
+    private final FlightService service;
 
-    @PostMapping(path = "/test")
-    public ResponseEntity<String> test() {
-        log.info("Request received");
-        return ResponseEntity.ok("I am OK");
+    @GetMapping(path = "/interconnections")
+    public ResponseEntity<List<Flight>> getFlight(@RequestParam(name = "departure") String departure,
+                                                  @RequestParam(name = "arrival") String arrival,
+                                                  @RequestParam(name = "departureDateTime") String departureDateTime,
+                                                  @RequestParam(name = "arrivalDateTime") String arrivalDateTime) {
+
+        return ResponseEntity.ok(service.getFlights(departure, arrival, departureDateTime, arrivalDateTime));
     }
 
 
--- src/test/java/com/ryanair/flight/controller/FlightsServiceControllerTest.java
+++ src/test/java/com/ryanair/flight/controller/FlightsServiceControllerTest.java
@@ -2,20 +2,39 @@ package com.ryanair.flight.controller;
 
 import com.jayway.restassured.RestAssured;
 import com.jayway.restassured.http.ContentType;
+import com.ryanair.flight.client.SchedulesApiClient;
+import com.ryanair.flight.config.RoutsApiClientConfig;
+import com.ryanair.flight.domain.Flight;
+import com.ryanair.flight.service.FlightService;
 import org.assertj.core.api.Assertions;
 import org.junit.jupiter.api.BeforeEach;
 import org.junit.jupiter.api.Test;
 import org.junit.jupiter.api.extension.ExtendWith;
+import org.mockito.Mockito;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.boot.test.mock.mockito.MockBean;
 import org.springframework.boot.web.server.LocalServerPort;
+import org.springframework.test.context.ContextConfiguration;
 import org.springframework.test.context.junit.jupiter.SpringExtension;
+
+import java.util.Arrays;
+import java.util.List;
 
 import static com.jayway.restassured.RestAssured.given;
 
 @ExtendWith(SpringExtension.class)
 @SpringBootTest(webEnvironment = SpringBootTest.WebEnvironment.RANDOM_PORT)
+@ContextConfiguration(classes = {RoutsApiClientConfig.class, SchedulesApiClient.class})
 class FlightsServiceControllerTest {
-    public static final String FLIGHTS = "/scheduledFlights";
+    private static final String DEPARTURE = "DUB";
+    private static final String ARRIVAL = "WROC";
+    private static final String DEPARTURE_DATA_TIME = "2018-03-01T07:00";
+    private static final String ARRIVAL_DATA_TIME="2018-03-03T21:00";
+
+    private static final String FLIGHTS = "/scheduledFlights";
+
+    @MockBean
+    private FlightService flightService;
 
     @LocalServerPort
     private int port;
@@ -27,16 +46,22 @@ class FlightsServiceControllerTest {
     }
 
     @Test
-    public void dummyTest() {
-        String response = given()
+    public void shouldReturnListOfLights() {
+        Flight flight = Flight.builder().build();
+        Mockito.when(flightService.getFlights(DEPARTURE, ARRIVAL, DEPARTURE_DATA_TIME, ARRIVAL_DATA_TIME))
+                .thenReturn(List.of(flight));
+
+        String requestUrl = "interconnections?departure=DUB&arrival=WRO&departureDateTime=2018-03-01T07:00&arrivalDateTime=2018-03-03T21:00";
+        List<Flight> flightList = Arrays.asList(given()
                 .when()
                 .contentType(ContentType.JSON)
-                .post("/test")
+                .get(requestUrl)
                 .then()
                 .assertThat()
                 .statusCode(200)
                 .extract()
-                .asString();
-        Assertions.assertThat(response).isEqualToIgnoringCase("I am Ok");
+                .body()
+                .as(Flight[].class));
+        Assertions.assertThat(flightList).containsExactlyInAnyOrder(flight);
     }
 }
