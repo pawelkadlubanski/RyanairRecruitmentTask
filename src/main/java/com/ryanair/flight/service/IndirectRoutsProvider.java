4195884da6b2a5a6bd63049bb27f422335d0d37b
Pawel Kadlubanski <pawel.kadlubanski@gmail.com>
2019-10-27 17:06:41 +0100

    Add maching leg for indirect routs.

---
+++ src/main/java/com/ryanair/flight/service/IndirectRoutsProvider.java
@@ -0,0 +1,41 @@
+package com.ryanair.flight.service;
+
+import com.ryanair.flight.domain.IndirectRout;
+import com.ryanair.flight.domain.Rout;
+import lombok.AllArgsConstructor;
+import lombok.extern.slf4j.Slf4j;
+import org.springframework.stereotype.Component;
+
+import java.util.List;
+import java.util.Optional;
+import java.util.stream.Collectors;
+
+@Slf4j
+@Component
+@AllArgsConstructor
+public class IndirectRoutsProvider {
+
+    public Optional<IndirectRout> matchSecondLeg(Rout firstLegRout, List<Rout> secondLegRoutsList) {
+        String firstLegArrival = firstLegRout.getAirportTo();
+        return secondLegRoutsList.stream()
+                .filter(rout -> rout.getAirportFrom().equals(firstLegArrival))
+                .findFirst()
+                .map(rout -> IndirectRout.builder().firstLeg(firstLegRout).secondLeg(rout).build());
+    }
+
+    public List<IndirectRout> matchRouts(String departure, String arrival, List<Rout> listOfAllRouts) {
+        List<Rout> firstLegRoutsList = listOfAllRouts.stream()
+                .filter(rout -> rout.getAirportFrom().equals(departure))
+                .collect(Collectors.toList());
+
+        List<Rout> secondLegRoutsList = listOfAllRouts.stream()
+                .filter(rout -> rout.getAirportTo().equals(arrival))
+                .collect(Collectors.toList());
+
+        return firstLegRoutsList.stream()
+                .map(rout -> matchSecondLeg(rout, secondLegRoutsList))
+                .filter(indirectRout -> indirectRout.isPresent())
+                .map(indirectRout -> indirectRout.get())
+                .collect(Collectors.toList());
+    }
+}
---
+++ src/test/java/com/ryanair/flight/service/IndirectRoutsProviderTest.java
@@ -0,0 +1,102 @@
+package com.ryanair.flight.service;
+
+import com.ryanair.flight.domain.IndirectRout;
+import com.ryanair.flight.domain.Rout;
+import org.assertj.core.api.Assertions;
+import org.junit.jupiter.api.Test;
+
+import java.util.List;
+
+class IndirectRoutsProviderTest {
+
+    private IndirectRoutsProvider provider = new IndirectRoutsProvider();
+
+    @Test
+    public void shouldReturnListOfIndirectRoutWhenThereIsOnlyOnePairOfRoutsWhichPass() {
+        Rout firstLegRout = Rout.builder()
+                .airportFrom("DUB")
+                .airportTo("STN")
+                .build();
+
+        Rout secondLegRout = Rout.builder()
+                .airportFrom("STN")
+                .airportTo("WROC")
+                .build();
+
+        List<Rout> listOfAllRouts = List.of(firstLegRout, secondLegRout);
+
+        List<IndirectRout> listOfIndirectRouts = provider.matchRouts("DUB", "WROC", listOfAllRouts);
+        Assertions.assertThat(listOfIndirectRouts).containsExactlyInAnyOrder(
+                IndirectRout.builder().firstLeg(firstLegRout).secondLeg(secondLegRout).build());
+    }
+
+    @Test
+    public void shouldReturnEmptyListOfIndirectRoutWhenThereIsNoPairOfRoutsWhichPass() {
+        Rout firstLegRout = Rout.builder()
+                .airportFrom("DUB")
+                .airportTo("CHQ")
+                .build();
+
+        Rout secondLegRout = Rout.builder()
+                .airportFrom("SKG")
+                .airportTo("WROC")
+                .build();
+
+        List<Rout> listOfAllRouts = List.of(firstLegRout, secondLegRout);
+
+        List<IndirectRout> listOfIndirectRouts = provider.matchRouts("DUB", "WROC", listOfAllRouts);
+        Assertions.assertThat(listOfIndirectRouts.isEmpty()).isTrue();
+    }
+
+    @Test
+    public void shouldReturnListOfIndirectRoutWhenThereIsOnPairOfRoutsWhichPassAndOneWhichNotPass() {
+        Rout firstPassingLegRout = Rout.builder()
+                .airportFrom("DUB")
+                .airportTo("STN")
+                .build();
+
+        Rout secondPassingLegRout = Rout.builder()
+                .airportFrom("STN")
+                .airportTo("WROC")
+                .build();
+
+        Rout firstNonPassingLegRout = Rout.builder()
+                .airportFrom("DUB")
+                .airportTo("CHQ")
+                .build();
+
+        Rout secondNonPassingLegRout = Rout.builder()
+                .airportFrom("SKG")
+                .airportTo("WROC")
+                .build();
+
+        List<IndirectRout> listOfIndirectRouts = provider.matchRouts("DUB", "WROC",
+                List.of(firstPassingLegRout, firstNonPassingLegRout, secondPassingLegRout, secondNonPassingLegRout));
+        Assertions.assertThat(listOfIndirectRouts).containsExactlyInAnyOrder(
+                IndirectRout.builder().firstLeg(firstPassingLegRout).secondLeg(secondPassingLegRout).build());
+    }
+
+    @Test
+    public void shouldReturnListOfIndirectRoutWhenThereIsOnlyOnePairOfRoutsWhichPassAnOneDirectFlightRout() {
+        Rout directRout = Rout.builder()
+                .airportFrom("DUB")
+                .airportTo("WROC")
+                .build();
+
+        Rout firstLegRout = Rout.builder()
+                .airportFrom("DUB")
+                .airportTo("STN")
+                .build();
+
+        Rout secondLegRout = Rout.builder()
+                .airportFrom("STN")
+                .airportTo("WROC")
+                .build();
+
+        List<Rout> listOfAllRouts = List.of(firstLegRout, secondLegRout, directRout);
+
+        List<IndirectRout> listOfIndirectRouts = provider.matchRouts("DUB", "WROC", listOfAllRouts);
+        Assertions.assertThat(listOfIndirectRouts).containsExactlyInAnyOrder(
+                IndirectRout.builder().firstLeg(firstLegRout).secondLeg(secondLegRout).build());
+    }
+ }
